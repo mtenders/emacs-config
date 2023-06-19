3aa3c2c883ee015eef023bfd7de100c29b9e8bcc
Michael T. Enders <michael@michael-enders.com>
2023-06-19 09:42:34 +0200

    autosave

--- early-init.el
+++ early-init.el
@@ -27,14 +27,14 @@
 (scroll-bar-mode -1)
 
 ;; Remove decorations
-(setq default-frame-alist
-      (append ; Note: if there are any conflicting settings in
-              ; ‘default-frame-alist’, it is the one that comes first that gets
-              ; applied .
-       '((undecorated . t)
-         (drag-internal-border . t)
-         (internal-border-width . 4))
-       default-frame-alist))
+;; (setq default-frame-alist
+;;       (append ; Note: if there are any conflicting settings in
+;;               ; ‘default-frame-alist’, it is the one that comes first that gets
+;;               ; applied .
+;;        '((undecorated . t)
+;;          (drag-internal-border . t)
+;;          (internal-border-width . 4))
+;;        default-frame-alist))
 
 ;; Disable startup screen
 (setq inhibit-startup-screen t)
