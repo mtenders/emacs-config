c0d66ab0d988c05f48bd673d4aa232b38bc0792e
Michael T. Enders <michael@michael-enders.com>
2025-01-09 10:52:07 +0100

    +impatient-mode for markdown live preview

--- init.el
+++ init.el
@@ -510,6 +510,16 @@
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
+;; https://stackoverflow.com/a/36189456
+(use-package impatient-mode
+  :config
+  (defun markdown-html (buffer)
+    (princ (with-current-buffer buffer
+             (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp
+theme=\"united\" style=\"display:none;\"> %s  </xmp><script
+src=\"http://ndossougbe.github.io/strapdown/dist/strapdown.js\"></script></html>"
+                     (buffer-substring-no-properties (point-min) (point-max))))
+           (current-buffer))))
 
 ;;------------------------------------------------------------------------------
 ;; LATEX
